db77a7f8397a8df7a36249a3c2db85c0f7764107
Abhishek Yadav <abhishek.yadav@harnstech.com>
2023-07-07 13:40:29 +0530

    node api

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.send("ABHISHEK YADAV");
+});
+
+app.listen(5000, () => {
+  console.log(`Server Started at ${5000}`);
+});
